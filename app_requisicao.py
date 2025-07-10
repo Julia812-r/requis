9cc8c4117e0bbda4d697428c9eea712b2f74b4e0
Julia812-r <sotilejulia07@gmail.com>
2025-07-10 00:39:36 -0300

    Update app_requisicao.py

--- app_requisicao.py
+++ app_requisicao.py
@@ -219,7 +219,7 @@ elif aba == "Conferir Status de Solicitação":
     if filtro_nome:
         df = df[df['Nome do Solicitante'].str.lower().str.contains(filtro_nome.lower())]
     if filtro_numero:
-        df = df[df['Número Solicitação'].str.upper() == filtro_numero.upper()]
+        df = df[df['numero solicitaçao'].str.upper() == filtro_numero.upper()]
 
     if df.empty:
         st.info("Nenhuma solicitação encontrada.")
@@ -324,7 +324,7 @@ elif aba == "Histórico (Acesso Restrito)":
         novo_numero_rc = st.text_input("Número da RC (deixe em branco para não alterar)")
 
         if st.button("Atualizar Status e RC"):
-            docs = list(db.collection("requisicoes").where("Número Solicitação", "==", numero_req_atualizar).stream())
+            docs = list(db.collection("requisicoes").where("numero solicitaçao", "==", numero_req_atualizar).stream())
             if docs:
                 for doc in docs:
                     update_data = {"Status": novo_status}
